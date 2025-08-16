cea2bdcf4acc4c40102995001f683bc69e25034d
Matthew Miller <606945+mmiller7@users.noreply.github.com>
2025-08-16 12:33:58 -0400

    Removed hard-coded path to top level directory

--- TPMS/TPMS-Helper.sh
+++ TPMS/TPMS-Helper.sh
@@ -11,54 +11,58 @@
 #
 #Is there a better way to do this? Probably, but it works.
 
-FILE=$HOME/TPMS/.tpms
+# Find the base directory to replace hard coded paths
+BASEDIR=`dirname "$0"`
+echo $BASEDIR
+
+FILE="$BASEDIR/.tpms"
 if test -f "$FILE"; then
     echo
 else
 
-REQUIRED_PKG="python3"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-  sudo apt-get --yes install $REQUIRED_PKG
-fi
+	REQUIRED_PKG="python3"
+	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+	echo Checking for $REQUIRED_PKG: $PKG_OK
+	if [ "" = "$PKG_OK" ]; then
+		echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+		sudo apt-get --yes install $REQUIRED_PKG
+	fi
 
-REQUIRED_PKG="rtl-433"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-  sudo apt-get --yes install $REQUIRED_PKG
-fi
+	REQUIRED_PKG="rtl-433"
+	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+	echo Checking for $REQUIRED_PKG: $PKG_OK
+	if [ "" = "$PKG_OK" ]; then
+		echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+		sudo apt-get --yes install $REQUIRED_PKG
+	fi
 
-REQUIRED_PKG="gnuradio"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-  sudo apt-get --yes install $REQUIRED_PKG
-fi
+	REQUIRED_PKG="gnuradio"
+	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+	echo Checking for $REQUIRED_PKG: $PKG_OK
+	if [ "" = "$PKG_OK" ]; then
+		echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+		sudo apt-get --yes install $REQUIRED_PKG
+	fi
 
-REQUIRED_PKG="sox"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-  sudo apt-get --yes install $REQUIRED_PKG
-fi
+	REQUIRED_PKG="sox"
+	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+	echo Checking for $REQUIRED_PKG: $PKG_OK
+	if [ "" = "$PKG_OK" ]; then
+		echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+		sudo apt-get --yes install $REQUIRED_PKG
+	fi
 
-REQUIRED_PKG="python3-crcmod"
-PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
-echo Checking for $REQUIRED_PKG: $PKG_OK
-if [ "" = "$PKG_OK" ]; then
-  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
-  sudo apt-get --yes install $REQUIRED_PKG
-fi
+	REQUIRED_PKG="python3-crcmod"
+	PKG_OK=$(dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+	echo Checking for $REQUIRED_PKG: $PKG_OK
+	if [ "" = "$PKG_OK" ]; then
+		echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+		sudo apt-get --yes install $REQUIRED_PKG
+	fi
 
-chmod +x /$HOME/TPMS/Resources/code_gen
+	chmod +x "$BASEDIR/Resources/code_gen"
 
-touch .tpms
+	touch .tpms
 
 fi
 
@@ -74,22 +78,22 @@ echo 5. Credits
 
 read num
 
-cd $HOME/TPMS/
+cd "$BASEDIR"
 
 case "$num" in
    "1") python3 Resources/PMV-107J.py 
    
-   Resources/./code_gen -s 250k -r $HOME/TPMS/Output/test.txt -w $HOME/TPMS/Output/pmv-107j.cu8
+   Resources/./code_gen -s 250k -r Output/test.txt -w Output/pmv-107j.cu8
    
-   rtl_433 -r $HOME/TPMS/Output/pmv-107j.cu8 -w $HOME/TPMS/Output/pmv-107j.ook
+   rtl_433 -r Output/pmv-107j.cu8 -w Output/pmv-107j.ook
    
-   python3 Resources/subghz_ook_to_sub.py $HOME/TPMS/Output/pmv-107j.ook 315000000
+   python3 Resources/subghz_ook_to_sub.py Output/pmv-107j.ook 315000000
    
-   rm $HOME/TPMS/Output/test.txt
+   rm Output/test.txt
    
-   rm $HOME/TPMS/Output/pmv-107j.cu8
+   rm Output/pmv-107j.cu8
    
-   rm $HOME/TPMS/Output/pmv-107j.ook
+   rm Output/pmv-107j.ook
    
    exit 1
    ;;
@@ -99,38 +103,38 @@ case "$num" in
    python3 Resources/tpms_citroen.py -i $id -r 1
    python3 Resources/tpms_citroen.py -i $id -r 2
    python3 Resources/tpms_citroen.py -i $id -r 3
-   python3 Resources/tpms_fsk.py -b 19200 -r $HOME/TPMS/Output/Citroen0.u8 -w $HOME/TPMS/Output/citroen0.cu8
-   python3 Resources/tpms_fsk.py -b 19200 -r $HOME/TPMS/Output/Citroen1.u8 -w $HOME/TPMS/Output/citroen1.cu8
-   python3 Resources/tpms_fsk.py -b 19200 -r $HOME/TPMS/Output/Citroen2.u8 -w $HOME/TPMS/Output/citroen2.cu8
-   python3 Resources/tpms_fsk.py -b 19200 -r $HOME/TPMS/Output/Citroen3.u8 -w $HOME/TPMS/Output/citroen3.cu8
+   python3 Resources/tpms_fsk.py -b 19200 -r Output/Citroen0.u8 -w Output/citroen0.cu8
+   python3 Resources/tpms_fsk.py -b 19200 -r Output/Citroen1.u8 -w Output/citroen1.cu8
+   python3 Resources/tpms_fsk.py -b 19200 -r Output/Citroen2.u8 -w Output/citroen2.cu8
+   python3 Resources/tpms_fsk.py -b 19200 -r Output/Citroen3.u8 -w Output/citroen3.cu8
    
-   dd bs=5000 count=1 if=/dev/zero | sox -t raw -v 0 -c2 -b8 -eunsigned-integer -r 250k - -t raw - > $HOME/TPMS/Output/citroen.cu8
-   cat $HOME/TPMS/Output/citroen0.cu8 >> $HOME/TPMS/Output/citroen.cu8
-   dd bs=5000 count=1 if=/dev/zero | sox -t raw -v 0 -c2 -b8 -eunsigned-integer -r 250k - -t raw - >> $HOME/TPMS/Output/citroen.cu8
-   cat $HOME/TPMS/Output/citroen1.cu8 >> $HOME/TPMS/Output/citroen.cu8
-   dd bs=5000 count=1 if=/dev/zero | sox -t raw -v 0 -c2 -b8 -eunsigned-integer -r 250k - -t raw - >> $HOME/TPMS/Output/citroen.cu8
-   cat $HOME/TPMS/Output/citroen2.cu8 >> $HOME/TPMS/Output/citroen.cu8
-   dd bs=5000 count=1 if=/dev/zero | sox -t raw -v 0 -c2 -b8 -eunsigned-integer -r 250k - -t raw - >> $HOME/TPMS/Output/citroen.cu8
-   cat $HOME/TPMS/Output/citroen3.cu8 >> $HOME/TPMS/Output/citroen.cu8
-   dd bs=5000 count=1 if=/dev/zero | sox -t raw -v 0 -c2 -b8 -eunsigned-integer -r 250k - -t raw - >> $HOME/TPMS/Output/citroen.cu8
+   dd bs=5000 count=1 if=/dev/zero | sox -t raw -v 0 -c2 -b8 -eunsigned-integer -r 250k - -t raw - > Output/citroen.cu8
+   cat Output/citroen0.cu8 >> Output/citroen.cu8
+   dd bs=5000 count=1 if=/dev/zero | sox -t raw -v 0 -c2 -b8 -eunsigned-integer -r 250k - -t raw - >> Output/citroen.cu8
+   cat Output/citroen1.cu8 >> Output/citroen.cu8
+   dd bs=5000 count=1 if=/dev/zero | sox -t raw -v 0 -c2 -b8 -eunsigned-integer -r 250k - -t raw - >> Output/citroen.cu8
+   cat Output/citroen2.cu8 >> Output/citroen.cu8
+   dd bs=5000 count=1 if=/dev/zero | sox -t raw -v 0 -c2 -b8 -eunsigned-integer -r 250k - -t raw - >> Output/citroen.cu8
+   cat Output/citroen3.cu8 >> Output/citroen.cu8
+   dd bs=5000 count=1 if=/dev/zero | sox -t raw -v 0 -c2 -b8 -eunsigned-integer -r 250k - -t raw - >> Output/citroen.cu8
    
-   rtl_433 -r $HOME/TPMS/Output/citroen.cu8 -w $HOME/TPMS/Output/citroen.ook
+   rtl_433 -r Output/citroen.cu8 -w Output/citroen.ook
    
-   python3 Resources/subghz_ook_to_sub.py $HOME/TPMS/Output/citroen.ook 315000000
+   python3 Resources/subghz_ook_to_sub.py Output/citroen.ook 315000000
    
-   rm $HOME/TPMS/Output/Citroen0.u8
-   rm $HOME/TPMS/Output/Citroen1.u8
-   rm $HOME/TPMS/Output/Citroen2.u8
-   rm $HOME/TPMS/Output/Citroen3.u8
+   rm Output/Citroen0.u8
+   rm Output/Citroen1.u8
+   rm Output/Citroen2.u8
+   rm Output/Citroen3.u8
    
-   rm $HOME/TPMS/Output/citroen0.cu8
-   rm $HOME/TPMS/Output/citroen1.cu8
-   rm $HOME/TPMS/Output/citroen2.cu8
-   rm $HOME/TPMS/Output/citroen3.cu8
+   rm Output/citroen0.cu8
+   rm Output/citroen1.cu8
+   rm Output/citroen2.cu8
+   rm Output/citroen3.cu8
    
-   rm $HOME/TPMS/Output/citroen.cu8
+   rm Output/citroen.cu8
    
-   rm $HOME/TPMS/Output/citroen.ook
+   rm Output/citroen.ook
    
    exit 1
    ;;
@@ -138,46 +142,46 @@ case "$num" in
    "3") echo Enter 8 digit Ford hex id
    read id
    python3 Resources/tpms_ford.py -i $id
-   python3 Resources/tpms_fsk.py -b 19200 -r $HOME/TPMS/Output/Ford.u8 -w $HOME/TPMS/Output/ford1.cu8
+   python3 Resources/tpms_fsk.py -b 19200 -r Output/Ford.u8 -w Output/ford1.cu8
    
-   dd bs=5000 count=1 if=/dev/zero | sox -t raw -v 0 -c2 -b8 -eunsigned-integer -r 250k - -t raw - > $HOME/TPMS/Output/ford.cu8
-   cat $HOME/TPMS/Output/ford1.cu8 >> $HOME/TPMS/Output/ford.cu8
-   dd bs=5000 count=1 if=/dev/zero | sox -t raw -v 0 -c2 -b8 -eunsigned-integer -r 250k - -t raw - >> $HOME/TPMS/Output/ford.cu8
-   cat $HOME/TPMS/Output/ford1.cu8 >> $HOME/TPMS/Output/ford.cu8
-   dd bs=5000 count=1 if=/dev/zero | sox -t raw -v 0 -c2 -b8 -eunsigned-integer -r 250k - -t raw - >> $HOME/TPMS/Output/ford.cu8
-   cat $HOME/TPMS/Output/ford1.cu8 >> $HOME/TPMS/Output/ford.cu8
-   dd bs=5000 count=1 if=/dev/zero | sox -t raw -v 0 -c2 -b8 -eunsigned-integer -r 250k - -t raw - >> $HOME/TPMS/Output/ford.cu8
-   cat $HOME/TPMS/Output/ford1.cu8 >> $HOME/TPMS/Output/ford.cu8
-   dd bs=5000 count=1 if=/dev/zero | sox -t raw -v 0 -c2 -b8 -eunsigned-integer -r 250k - -t raw - >> $HOME/TPMS/Output/ford.cu8
+   dd bs=5000 count=1 if=/dev/zero | sox -t raw -v 0 -c2 -b8 -eunsigned-integer -r 250k - -t raw - > Output/ford.cu8
+   cat Output/ford1.cu8 >> Output/ford.cu8
+   dd bs=5000 count=1 if=/dev/zero | sox -t raw -v 0 -c2 -b8 -eunsigned-integer -r 250k - -t raw - >> Output/ford.cu8
+   cat Output/ford1.cu8 >> Output/ford.cu8
+   dd bs=5000 count=1 if=/dev/zero | sox -t raw -v 0 -c2 -b8 -eunsigned-integer -r 250k - -t raw - >> Output/ford.cu8
+   cat Output/ford1.cu8 >> Output/ford.cu8
+   dd bs=5000 count=1 if=/dev/zero | sox -t raw -v 0 -c2 -b8 -eunsigned-integer -r 250k - -t raw - >> Output/ford.cu8
+   cat Output/ford1.cu8 >> Output/ford.cu8
+   dd bs=5000 count=1 if=/dev/zero | sox -t raw -v 0 -c2 -b8 -eunsigned-integer -r 250k - -t raw - >> Output/ford.cu8
    
-   rtl_433 -r $HOME/TPMS/Output/ford.cu8 -w $HOME/TPMS/Output/ford.ook
+   rtl_433 -r Output/ford.cu8 -w Output/ford.ook
    
-   python3 Resources/subghz_ook_to_sub.py $HOME/TPMS/Output/ford.ook 315000000
+   python3 Resources/subghz_ook_to_sub.py Output/ford.ook 315000000
    
-   rm $HOME/TPMS/Output/Ford.u8
+   rm Output/Ford.u8
    
-   rm $HOME/TPMS/Output/ford1.cu8
+   rm Output/ford1.cu8
    
-   rm $HOME/TPMS/Output/ford.cu8
+   rm Output/ford.cu8
    
-   rm $HOME/TPMS/Output/ford.ook
+   rm Output/ford.ook
    
    exit 1
    ;;
    
    "4")python3 Resources/tpms_smd3ma4.py 
    
-   Resources/./code_gen -s 250k -r $HOME/TPMS/Output/smd3ma4.txt -w $HOME/TPMS/Output/smd3ma4.cu8
+   Resources/./code_gen -s 250k -r Output/smd3ma4.txt -w Output/smd3ma4.cu8
    
-   rtl_433 -r $HOME/TPMS/Output/smd3ma4.cu8 -w $HOME/TPMS/Output/smd3ma4.ook
+   rtl_433 -r Output/smd3ma4.cu8 -w Output/smd3ma4.ook
    
-   python3 Resources/subghz_ook_to_sub.py $HOME/TPMS/Output/smd3ma4.ook 315000000
+   python3 Resources/subghz_ook_to_sub.py Output/smd3ma4.ook 315000000
    
-   rm $HOME/TPMS/Output/smd3ma4.txt
+   rm Output/smd3ma4.txt
    
-   rm $HOME/TPMS/Output/smd3ma4.cu8
+   rm Output/smd3ma4.cu8
    
-   rm $HOME/TPMS/Output/smd3ma4.ook
+   rm Output/smd3ma4.ook
    
    exit 1
    ;;
